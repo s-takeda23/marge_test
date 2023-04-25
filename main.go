b2584936bcfaec604f938463f35a339b39b174b8
Naoki Fukasawa <fukasawa@housei-inc.com>
2023-04-25 18:05:50 +0900

    test3

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 /*
  * @Author: Naoki Fukasawa fukasawa@housei-inc.com
  * @Date: 2023-04-25 17:50:04
- * @LastEditors: ShotaTakeda s-takeda@housei-inc.com
- * @LastEditTime: 2023-04-25 18:03:55
+ * @LastEditors: Naoki Fukasawa fukasawa@housei-inc.com
+ * @LastEditTime: 2023-04-25 18:05:42
  * @FilePath: \marge_test\main.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,6 +28,7 @@ func Test() {
 func fukasawa_second() {
 	return
 }
+
 // add testfunc
 func test() {
 	fmt.Println("test")
@@ -36,3 +37,6 @@ func test() {
 // test2
 
 // test3
+func test3() {
+
+}
