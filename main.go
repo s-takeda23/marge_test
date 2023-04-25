a0894e553ca12d807e93e133fd62cfd371698fa5
Naoki Fukasawa <fukasawa@housei-inc.com>
2023-04-25 17:51:07 +0900

    add fukasawa

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: Naoki Fukasawa fukasawa@housei-inc.com
+ * @Date: 2023-04-25 17:50:04
+ * @LastEditors: Naoki Fukasawa fukasawa@housei-inc.com
+ * @LastEditTime: 2023-04-25 17:50:58
+ * @FilePath: \marge_test\main.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package main
 
 import "fmt"
@@ -7,3 +15,6 @@ func main() {
 }
 
 // test
+func fukasawa() {
+	return
+}
